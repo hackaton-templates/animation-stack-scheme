972e5549d2809b393b78937023b6c7e0c9482b10
StarPandaBeg <kshmurakov@mail.ru>
2024-05-22 19:45:32 +0300

    feat: add global style
    add roboto font

--- src/project.ts
+++ src/project.ts
@@ -1,6 +1,7 @@
-import {makeProject} from '@motion-canvas/core';
+import { makeProject } from "@motion-canvas/core";
 
-import example from './scenes/example?scene';
+import example from "./scenes/example?scene";
+import "./global.css";
 
 export default makeProject({
   scenes: [example],
