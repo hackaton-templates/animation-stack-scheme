847711c6571e087c7a975f67e3644bd746ed85b2
StarPandaBeg <kshmurakov@mail.ru>
2024-05-22 20:44:49 +0300

    feat: add animated grid helper

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const GridConfig = {
+  stroke: "#cccccc",
+};
---
+++ src/util/grid.tsx
@@ -0,0 +1,28 @@
+import { type Node, Grid } from "@motion-canvas/2d";
+import { all, createRef, easeInOutCubic, waitUntil } from "@motion-canvas/core";
+import { GridConfig } from "../config";
+
+export function* createGrid(parent: Node) {
+  const grid = createRef<Grid>();
+
+  parent.add(
+    <Grid
+      ref={grid}
+      width={"100%"}
+      height={"100%"}
+      spacing={60}
+      stroke={GridConfig.stroke}
+      start={0.5}
+      end={0.5}
+      opacity={0.5}
+    />
+  );
+
+  yield* all(
+    grid().opacity(1, 1, easeInOutCubic),
+    grid().start(1, 0.8, easeInOutCubic),
+    grid().end(0, 0.8, easeInOutCubic)
+  );
+  yield* waitUntil("start");
+  return grid;
+}
