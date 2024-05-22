7e53aa32c9f8ecebc14a9ea9e6aa1d8beaf12149
StarPandaBeg <kshmurakov@mail.ru>
2024-05-22 19:44:37 +0300

    Initial

---
+++ src/scenes/example.tsx
@@ -0,0 +1,10 @@
+import {Circle, makeScene2D} from '@motion-canvas/2d';
+import {createRef} from '@motion-canvas/core';
+
+export default makeScene2D(function* (view) {
+  const circle = createRef<Circle>();
+
+  view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
+
+  yield* circle().scale(2, 2).to(1, 2);
+});
