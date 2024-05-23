d08a7006c2b346c66b9ef0d753552c089e7d3ff5
StarPandaBeg <kshmurakov@mail.ru>
2024-05-24 00:49:36 +0300

    feat: add import aliases for src, assets, src/components

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import {defineConfig} from 'vite';
-import motionCanvas from '@motion-canvas/vite-plugin';
-import ffmpeg from '@motion-canvas/ffmpeg';
+import { defineConfig } from "vite";
+import motionCanvas from "@motion-canvas/vite-plugin";
+import ffmpeg from "@motion-canvas/ffmpeg";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     motionCanvas(),
     ffmpeg(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
 });
