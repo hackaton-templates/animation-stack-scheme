20e5e539a8a51639f53623cab7ef1c140a9b3428
StarPandaBeg <kshmurakov@mail.ru>
2024-05-23 14:06:27 +0300

    feat: add schema-group component

---
+++ src/components/schema-group.tsx
@@ -0,0 +1,25 @@
+import { Layout } from "@motion-canvas/2d";
+import SchemaNode, { type SchemaNodeProps } from "./schema-node";
+import SchemaNodeHeader from "./schema-node-header";
+
+type SchemaGroupProps = SchemaNodeProps & {
+  header?: string;
+};
+
+export default class SchemaGroup extends SchemaNode {
+  constructor(props?: SchemaGroupProps) {
+    super({ ...props, children: null });
+    this.rectBase.fill("#f0f0f0");
+    this.rectBase.gap(20);
+
+    if (props.header) {
+      this.rectBase.add(<SchemaNodeHeader>{props.header}</SchemaNodeHeader>);
+    }
+
+    this.rectBase.add(
+      <Layout layout gap={10}>
+        {props.children}
+      </Layout>
+    );
+  }
+}
