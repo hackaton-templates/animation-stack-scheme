e8f55cd75a7131cdab2082a922c114865a693595
StarPandaBeg <kshmurakov@mail.ru>
2024-05-23 14:50:09 +0300

    feat: add schema-node stack animation

--- src/components/schema-node.tsx
+++ src/components/schema-node.tsx
@@ -1,17 +1,28 @@
 import { Node, NodeProps, Rect } from "@motion-canvas/2d";
-import { all, easeInOutElastic, makeRef } from "@motion-canvas/core";
+import {
+  ReferenceArray,
+  all,
+  createRefArray,
+  createSignal,
+  easeInOutElastic,
+  easeOutElastic,
+  makeRef,
+} from "@motion-canvas/core";
 import { SchemaNodeConfig } from "../config";
 
 export type SchemaNodeProps = NodeProps & {
   nodeType?: "default" | "api";
   instant?: boolean;
+  stack?: boolean;
 };
 
 export default class SchemaNode extends Node {
   public readonly rectBase: Rect;
+  public readonly stack: ReferenceArray<Rect>;
 
   constructor(props?: SchemaNodeProps) {
     super({ ...props, children: null });
+    this.stack = createRefArray<Rect>();
 
     const nodeType = props?.nodeType ?? "default";
     const fill = SchemaNodeConfig.background[nodeType];
@@ -37,12 +48,57 @@ export default class SchemaNode extends Node {
         {props.children}
       </Rect>
     );
+
+    if (props.stack) {
+      const sizeSignal = createSignal(() => {
+        return this.rectBase.size();
+      });
+      this.add(
+        <Rect
+          ref={this.stack}
+          width={sizeSignal().x}
+          height={sizeSignal().y}
+          fill={fill}
+          stroke="#000"
+          lineDash={lineDash}
+          lineWidth={2}
+          smoothCorners
+          radius={8}
+          zIndex={-1}
+          opacity={0}
+        />
+      );
+      this.add(
+        <Rect
+          ref={this.stack}
+          width={sizeSignal().x}
+          height={sizeSignal().y}
+          fill={fill}
+          stroke="#000"
+          lineDash={lineDash}
+          lineWidth={2}
+          smoothCorners
+          radius={8}
+          zIndex={-2}
+          opacity={0}
+        />
+      );
+    }
   }
 
   public *animate() {
     yield* all(
       this.rectBase.scale(1, 0.8, easeInOutElastic),
       this.rectBase.opacity(1, 0.8)
+    );
+  }
+
+  public *animateStack() {
+    yield* all(...this.stack.map((s) => s.opacity(1, 0)));
+    yield* all(
+      ...this.stack.map((s, i) =>
+        s.position([8 * (i + 1), 8 * (i + 1)], 0.5, easeOutElastic)
+      )
     );
   }
 }
